a21db89b3d46276c6d7beb184c62697a48af572b
joao <joaovictormelodonascimento@gmail.com>
2024-03-19 12:38:05 -0300

    adicao da funcao soma

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculate(operation) {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var result;
+
+    switch(operation) {
+        case '+':
+            result = Number(num1) + Number(num2);
+            break;
+    }
+
+    document.getElementById('result').innerHTML = 'Resultado: ' + result;
+}
